81a2c051122c5b14296fb171dd46c4bca5632be1
tlestang <t.lestang@imperial.ac.uk>
2023-07-12 21:19:16 +0100

    nvfotran: Must manually allocate polymorphic object to dynamic type

--- main.f90
+++ main.f90
@@ -7,7 +7,13 @@ program main
   type(cpublock) :: blk
 
   blk = cpublock(16)
-  obj = cputype()
+
+  allocate(cputype :: obj)
+  obj = cputype() ! This should automatically trigger
+  ! allocation of obj. It does for gfortran (9.4), but not for
+  ! nvfortran (22.5), which segfaults. Therefore obj is manually
+  ! allocated. This might be a problem since allocation must know
+  ! the dynamic type of obj.
 
   write(*,*) obj%doit(blk)
 end program main
